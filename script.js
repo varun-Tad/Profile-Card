a86ed26ededaecd1bac146b4db6f2131cac141bb
varun-Tad <varuntad141@gmail.com>
2022-01-19 16:42:33 +0530

    profile card

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttonThree = document.querySelector("#btn-three");
+const moreOption = document.querySelector("#more-option");
+
+
+
+buttonThree.addEventListener('click',function(){
+
+     moreOption.style.display="block";
+})
